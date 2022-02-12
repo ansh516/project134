94d026454af58d5e52e6bd9e6c25dfc2ef168f63
ansh516 <77134391+ansh516@users.noreply.github.com>
2022-02-11 16:18:47 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+status1 = "";
+img = "";
+objects = [];
+function preload() {
+   img = loadImage('baby.jpg'); 
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);  
+} 
+function draw() {
+    image(img, 0, 0, 640, 420);
+    
+  
+    if (status1 !="") 
+    {
+   for ( i = 0; i < objects.length; i++) 
+   {
+     document.getElementById("status").innerHTML= "Status : Object Detected";
+     fill("#0000FF");
+     percent = floor(objects[i].confidence * 100);
+     text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[1].y + 15);
+     noFill();
+     stroke("#0000FF");document
+     rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+     
+     
+   }
+     
+   } 
+    }
+
+
+function modelLoaded() {
+ console.log("Model Loaded!");
+  status1 = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
